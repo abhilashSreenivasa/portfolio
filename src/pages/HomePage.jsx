a11f8936f56671ba015a0e5b9f40c11c169bf617
Abhilash Sreenivasa <abhilash.sreenivasa@gmail.com>
2023-09-10 17:23:49 -0700

    Added about section,identity section closes #2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,17 +11,21 @@ function Home() {
       <div className='main-content'>
         <img src="headshot.jpeg" id='headshot'></img>
         <div className='main-content-links'>
+             <div className='main-content-links-data'>
+                <img className='icon' src='icons/email.png'></img>
+                <span>Email : <a className='a-link' href='mailto:abhilash.sreenivasa@gmail.com'> abhilash.sreenivasa@gmail.com </a></span>        
+            </div>
             <div className='main-content-links-data'>
                 <img className='icon' src='icons/github-sign.png'></img>
-                <span> Github : github.com/abhilashSreenivasa </span>        
+                <span>Github : <a className='a-link' href='https://github.com/abhilashSreenivasa/'>  github.com/abhilashSreenivasa </a></span>        
             </div>
             <div className='main-content-links-data'>
                 <img className='icon' src='icons/linkedin.png'></img>
-                <span> Linkedin : ln/abhilashSreenivasa </span>        
+                <span> Linkedin :<a className='a-link' href='https://www.linkedin.com/in/abhilash-sreenivasa-1b5256177/'> ln/abhilashSreenivasa</a> </span>        
             </div>
             <div className='main-content-links-data'>
                 <img className='icon' src='icons/instagram.png'></img>
-                <span> Instagram : instagram.com/abhilashSreenivasa </span>        
+                <span> Instagram :<a className='a-link' href='https://www.instagram.com/abhilash_sreeenivas/'> instagram.com/abhilashSreenivasa </a> </span>        
             </div>
             <hr className='hr-small'/>
             <div className='main-content-links-data'>
@@ -32,11 +36,17 @@ function Home() {
                 <img className='icon' src='icons/mortarboard.png'></img>
                 <span> Masters in Computer Science</span>        
             </div>
-            
-
         </div>
+
       </div>
-      
+      <div className='header-h3'>About</div>
+      <hr className='hr-small'/>
+      <div>
+        <div className='info-text'><i>Hi, I'm Abhilash Sreenivasa, a passionate Computer Science Master's student.
+With expertise in full-stack development, I bring hands-on skills in web technologies and a strong foundation in computer science principles.
+I'm excited to embark on new challenges in the tech industry and look forward to graduating this December. Let's build something great together!
+</i></div>
+      </div>
     </div>
   );
 }
